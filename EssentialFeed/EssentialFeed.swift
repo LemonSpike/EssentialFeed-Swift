b542ec52ce394f13f8e3bdc815cc3a6e68853837
Pranav Kasetti <pranavkasetti@gmail.com>
2025-08-23 18:27:01 +0100

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.swift
@@ -0,0 +1,9 @@
+//
+//  EssentialFeed.swift
+//  EssentialFeed
+//
+//  Created by Pranav Kasetti on 23/08/2025.
+//
+
+import Foundation
+
---
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Pranav Kasetti on 23/08/2025.
+//
+
+import Testing
+@testable import EssentialFeed
+
+struct EssentialFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
