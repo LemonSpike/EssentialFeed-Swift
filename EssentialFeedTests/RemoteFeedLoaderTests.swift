b53f49fd8401f825e52c06b4e38f4da1c06b3ab7
Pranav Kasetti <pranavkasetti@gmail.com>
2025-08-26 21:14:26 +0100

    Lint

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -63,11 +63,11 @@ struct RemoteFeedLoaderTests {
   }
 
   private class HTTPClientSpy: HTTPClient {
+    private var messages: [(url: URL, completion: (HTTPClientResult) -> Void)] = []
+
     var requestedURLs: [URL] {
       return messages.map { $0.url }
     }
-
-    private var messages: [(url: URL, completion: (HTTPClientResult) -> Void)] = []
 
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
       messages.append((url, completion))
