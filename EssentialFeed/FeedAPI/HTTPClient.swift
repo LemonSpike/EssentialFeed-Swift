8d5aa9194c7d6a4380c3406c8dba184ec8473ec1
Pranav Kasetti <pranavkasetti@gmail.com>
2025-08-31 18:41:59 +0100

    Move `HTTPClient` to a new file.

---
+++ EssentialFeed/FeedAPI/HTTPClient.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public protocol HTTPClient {
+  func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
+
+public enum HTTPClientResult {
+  case success(Data, HTTPURLResponse)
+  case failure(Error)
+}
