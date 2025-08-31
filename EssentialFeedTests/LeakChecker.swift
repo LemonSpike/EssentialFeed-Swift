54e9d034a07222dff9112a3e74da2fe602b313af
Pranav Kasetti <pranavkasetti@gmail.com>
2025-08-31 21:35:17 +0100

    Create `LeakChecker.swift`.

---
+++ EssentialFeedTests/LeakChecker.swift
@@ -0,0 +1,59 @@
+import Testing
+
+/// Checks for memory leaks when going out of scope.
+/// Taken from https://github.com/hakkurishian/swift_experiments/blob/master/MemoryLeakChecking/LeakChecker.swift.
+final class LeakChecker {
+  typealias Checkable = AnyObject & Sendable
+
+  @discardableResult
+  func checkForMemoryLeak<T: Checkable>(
+    fileID: String = #fileID,
+    filePath: String = #filePath,
+    line: Int = #line,
+    column: Int = #column,
+    _ instanceFactory: @autoclosure () -> T
+  ) -> T
+  {
+    let instance = instanceFactory()
+    let location = SourceLocation(fileID: fileID, filePath: filePath, line: line, column: column)
+    checks.append(
+      LeakCheck(
+        instance,
+        sourceLocation: location
+      )
+    )
+    return instance
+  }
+
+  private struct LeakCheck {
+    let sourceLocation: SourceLocation
+    private weak var weakReference: Checkable?
+    var isLeaking: Bool { weakReference != nil }
+    init(_ weakReference: Checkable, sourceLocation: SourceLocation) {
+      self.weakReference = weakReference
+      self.sourceLocation = sourceLocation
+    }
+  }
+
+  private var checks: [LeakCheck] = []
+
+  typealias Scope = (_ checker: LeakChecker) -> Void
+
+  private let scope: Scope
+
+  @discardableResult
+  init(scope: @escaping Scope) {
+    self.scope = scope
+    scope(self)
+  }
+
+  deinit {
+    for check in checks {
+      #expect(
+        !check.isLeaking,
+        "Instance should have been deallocated: Potential Memory Leak detected",
+        sourceLocation: check.sourceLocation
+      )
+    }
+  }
+}
