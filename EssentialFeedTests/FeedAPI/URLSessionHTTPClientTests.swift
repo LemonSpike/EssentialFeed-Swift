f2f5a160515aaa0f53c67d6a504d8f640149386c
Pranav Kasetti <pranavkasetti@gmail.com>
2025-09-09 21:55:48 +0100

    Extract duplicated code into a shared reusable helper method to simplify tests.

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -32,21 +32,11 @@ class URLSessionHTTPClientTests {
   @Test func testGetFromURLFailsOnRequestError() async throws {
     try await LeakChecker { [weak self] checker in
       guard let self else { return }
-      let error = NSError(domain: "Any Error", code: 1)
-      URLProtocolStub.stub(
-        data: nil,
-        response: nil,
-        error: error
-      )
+      let requestError = NSError(domain: "Any Error", code: 1)
+      let receivedError = try await resultErrorFor(data: nil, response: nil, error: requestError) as? NSError
       
-      let result = try await self.makeSUT().get(from: self.anyURL())
-      switch result {
-      case let .failure(receivedError as NSError):
-        #expect(receivedError.domain == error.domain)
-        #expect(receivedError.code == error.code)
-      default:
-        #expect(Bool(false), "Expected failure with error \(error), got \(result) instead.")
-      }
+      #expect(receivedError?.domain == requestError.domain)
+      #expect(receivedError?.code == requestError.code)
     }
   }
   
@@ -55,12 +45,56 @@ class URLSessionHTTPClientTests {
     
   // MARK: Helpers
   
-  private func makeSUT() -> URLSessionHTTPClient {
+  private func makeSUT(
+    fileID: String = #fileID,
+    filePath: String = #filePath,
+    line: Int = #line,
+    column: Int = #column
+  ) -> URLSessionHTTPClient {
     URLSessionHTTPClient()
   }
   
   private func anyURL() -> URL {
     URL(string: "https://any-url.com")!
+  }
+  
+  private func resultErrorFor(
+    data: Data?,
+    response: URLResponse?,
+    error: Error?,
+    fileID: String = #fileID,
+    filePath: String = #filePath,
+    line: Int = #line,
+    column: Int = #column
+  ) async throws -> Error? {
+    URLProtocolStub.stub(
+      data: data,
+      response: response,
+      error: error
+    )
+    
+    let sut = self.makeSUT(
+      fileID: fileID,
+      filePath: filePath,
+      line: line,
+      column: column
+    )
+    
+    var receivedError: Error?
+    let result = try await sut.get(from: self.anyURL())
+    switch result {
+    case let .failure(error as NSError):
+      receivedError = error
+    default:
+      let location = SourceLocation(
+        fileID: fileID,
+        filePath: filePath,
+        line: line,
+        column: column
+      )
+      #expect(Bool(false), "Expected failure with error \(error), got \(result) instead.", sourceLocation: location)
+    }
+    return receivedError
   }
   
   private class URLProtocolStub: URLProtocol {
