8d18eba487639ef1a2b80e2af3bf892148cfd5c5
Pranav Kasetti <pranavkasetti@gmail.com>
2025-10-11 18:08:18 +0100

    Add stub for `LoadFeedFromCacheUseCaseTests`.

---
+++ EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+
+final class LoadFeedFromCacheUseCaseTests: XCTestCase {
+  
+  override func setUpWithError() throws {
+  }
+  
+  override func tearDownWithError() throws {
+  }
+  
+  func testExample() throws {
+  }
+}
