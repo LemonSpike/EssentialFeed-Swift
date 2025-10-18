faa45ef200cdf70ca1c493317ac0c362596f9b34
Pranav Kasetti <pranavkasetti@gmail.com>
2025-10-18 14:30:44 +0100

    Make `FeedCachePolicy` static since it doesn't keep any state.

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -1,13 +1,15 @@
 import Foundation
 
 private final class FeedCachePolicy {
-  private let calendar = Calendar(identifier: .gregorian)
+  private init() {}
 
-  private var maxCacheAgeInDays: Int {
+  private static let calendar = Calendar(identifier: .gregorian)
+
+  private static var maxCacheAgeInDays: Int {
     7
   }
 
-  func validate(_ timestamp: Date, against date: Date) -> Bool {
+  static func validate(_ timestamp: Date, against date: Date) -> Bool {
     guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
       return false
     }
@@ -18,7 +20,6 @@ private final class FeedCachePolicy {
 public final class LocalFeedLoader {
   private let store: FeedStore
   private let currentDate: () -> Date
-  private let cachePolicy = FeedCachePolicy()
 
   public init(store: FeedStore, currentDate: @escaping () -> Date) {
     self.store = store
@@ -64,7 +65,7 @@ extension LocalFeedLoader: FeedLoader {
       switch result {
         case let .failure(error):
           completion(.failure(error))
-        case let .found(feed, timestamp) where self.cachePolicy.validate(timestamp, against: currentDate()):
+        case let .found(feed, timestamp) where FeedCachePolicy.validate(timestamp, against: currentDate()):
           completion(.success(feed.toModels()))
         case .found, .empty:
           completion(.success([]))
@@ -80,7 +81,7 @@ extension LocalFeedLoader {
       switch result {
         case .failure:
           store.deleteCachedFeed { _ in }
-        case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp, against: currentDate()):
+        case let .found(_, timestamp) where !FeedCachePolicy.validate(timestamp, against: currentDate()):
           store.deleteCachedFeed { _ in }
         case .found, .empty:
           break
