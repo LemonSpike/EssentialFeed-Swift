892856d18a0c90455be3ff5ee42b0b4ebf8f4c0c
Pranav Kasetti <pranavkasetti@gmail.com>
2025-10-31 20:58:11 +0000

    Create empty `CoreDataFeedStoreTests` based on `FailableFeedStore` specs.

---
+++ EssentialFeed/FeedCache/CoreData/CoreDataFeedStore.swift
@@ -0,0 +1,27 @@
+import CoreData
+import Foundation
+
+public class CoreDataFeedStore: FeedStore {
+  
+  private let managedObjectModel: NSManagedObjectModel?
+  private let bundle: Bundle
+  
+  public init(storeURL: URL, bundle: Bundle = .main) throws {
+    self.managedObjectModel = NSManagedObjectModel(contentsOf: storeURL)
+    self.bundle = bundle
+  }
+
+  
+  public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+    
+  }
+  
+  public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+    
+  }
+  
+  public func retrieve(completion: @escaping RetrievalCompletion) {
+    
+  }
+}
+
---
+++ EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,100 @@
+import EssentialFeed
+import Foundation
+import XCTest
+
+final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStore {
+  
+  override func setUp() {
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testRetrieveDeliversEmptyOnEmptyCache() {
+    
+  }
+  
+  func testRetrieveHasNoSideEffectsOnEmptyCache() {
+    
+  }
+  
+  func testRetrieveDeliversFoundValuesOnNonEmptyCache() {
+    
+  }
+  
+  func testRetrieveHasNoSideEffectsOnNonEmptyCache() {
+    
+  }
+  
+  func testRetrieveDeliversFailureOnRetrievalError() {
+    
+  }
+  
+  func testRetrieveHasNoSideEffectsOnFailure() {
+    
+  }
+  
+  func testInsertDeliversNoErrorOnEmptyCache() {
+    
+  }
+  
+  func testInsertDeliversNoErrorOnNonEmptyCache() {
+    
+  }
+  
+  func testInsertOverridesPreviouslyInsertedCacheValues() {
+    
+  }
+  
+  func testInsertDeliversErrorOnInsertionError() {
+    
+  }
+  
+  func testInsertHasNoSideEffectsOnInsertionError() {
+    
+  }
+  
+  func testDeleteDeliversNoErrorOnEmptyCache() {
+    
+  }
+  
+  func testDeleteHasNoSideEffectsOnEmptyCache() {
+    
+  }
+  
+  func testDeleteDeliversNoErrorOnNonEmptyCache() {
+    
+  }
+  
+  func testDeleteEmptiesPreviouslyInsertedCache() {
+    
+  }
+  
+  func testDeleteDeliversErrorOnDeletionError() {
+    
+  }
+  
+  func testDeleteHasNoSideEffectsOnDeletionError() {
+    
+  }
+  
+  func testStoreSideEffectsRunSerially() {
+    
+  }
+  
+  // - MARK: Helpers
+  
+  private func makeSUT(
+    storeURL: URL? = nil,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) -> FeedStore {
+    let storeBundle = Bundle(for: CoreDataFeedStore.self)
+    let storeURL = URL(fileURLWithPath: "/dev/null")
+    let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+    trackForMemoryLeaks(sut, file: file, line: line)
+    return sut
+  }
+}
