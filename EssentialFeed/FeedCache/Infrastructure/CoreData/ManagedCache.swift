760348358fcaade706291608693edd03d62c215b
Pranav Kasetti <pranavkasetti@gmail.com>
2025-11-08 22:05:03 +0000

    Fix force unwrap

--- EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedCache.swift
@@ -8,7 +8,10 @@ class ManagedCache: NSManagedObject {
 
 extension ManagedCache {
   static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
-    let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+    guard let name = entity().name else {
+      return nil
+    }
+    let request = NSFetchRequest<ManagedCache>(entityName: name)
     request.returnsObjectsAsFaults = false
     return try context.fetch(request).first
   }
