2010e484a3ba572f27a264211018e395e5702407
Pranav Kasetti <pranavkasetti@gmail.com>
2025-11-22 18:55:39 +0000

    Simplify `URLSessionHTTPClient` completion code with the new `Result` APIs.

--- EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
+++ EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -2,22 +2,24 @@ import Foundation
 
 public struct URLSessionHTTPClient: HTTPClient {
   private let session: URLSession
-  
+
   public init(session: URLSession = .shared) {
     self.session = session
   }
-  
+
   private struct UnexpectedValuesRepresentation: Error {}
-  
+
   public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
     session.dataTask(with: url) { data, response, error in
-      if let error {
-        completion(.failure(error))
-      } else if let data, let response = response as? HTTPURLResponse {
-        completion(.success((data, response)))
-      } else {
-        completion(.failure(UnexpectedValuesRepresentation()))
-      }
+      completion(Result {
+        if let error {
+          throw error
+        } else if let data, let response = response as? HTTPURLResponse {
+          return (data, response)
+        } else {
+          throw UnexpectedValuesRepresentation()
+        }
+      })
     }.resume()
   }
 }
