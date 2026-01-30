88dad4b70f4d0277faa8550ad16bcfde3844b4af
Pranav Kasetti <pranavkasetti@gmail.com>
2026-01-30 08:03:59 +0000

    Decouple `FeedImageViewModel` from UIKit dependency and update ui composer to inject imageTransformer. Update shimmer logic as well.

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -16,7 +16,12 @@ public final class FeedUIComposer {
   private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
     return { [weak controller] feed in
       controller?.tableModel = feed.map { model in
-        FeedImageCellController(model: model, imageLoader: loader)
+        FeedImageCellController(
+          viewModel: FeedImageViewModel(
+            model: model,
+            imageLoader: loader,
+            imageTransformer: UIImage.init)
+        )
       }
     }
   }
