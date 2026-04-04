a982394ca3104c1f004850ed7e657b41cf95aa8d
Pranav Kasetti <pranavkasetti@gmail.com>
2026-04-04 11:08:44 +0100

    Move production types to the production `EssentialFeed` module and make necessary APIs public.

---
+++ EssentialFeed/FeedPresentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public struct FeedErrorViewModel {
+  public let message: String?
+  
+  static var noError: FeedErrorViewModel {
+    FeedErrorViewModel(message: nil)
+  }
+  
+  static func error(message: String) -> FeedErrorViewModel {
+    FeedErrorViewModel(message: message)
+  }
+}
